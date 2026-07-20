2e4abf2847fe77a44ae968868d4597bad4c5646f
Hamna-21 <hamnamushtaq97@gmail.com>
2026-07-20 20:06:41 +0500

    removed agents

--- frontend/src/components/common/Navbar/components/DesktopNav.tsx
+++ frontend/src/components/common/Navbar/components/DesktopNav.tsx
@@ -22,35 +22,32 @@ export default function DesktopNav({ navLinks, handleLinkClick }: DesktopNavProp
 
   return (
     <button
-      key={link.href}
-      onClick={() => handleLinkClick(link.href)}
-      disabled={link.disabled}
-     className="group relative flex items-center justify-center px-9.5  py-2.5 rounded-lg bg-white/5 backdrop-blur-xl
-        border
-        border-white/10
-        text-white/80
-        font-medium
-        tracking-wide
-        hover:bg-red-500/10
-        hover:border-red-500/40
-        hover:text-white
-        hover:-translate-y-1
-        hover:shadow-lg
-        hover:shadow-red-500/20
-        transition-all
-        duration-300
-        disabled:opacity-50
-        disabled:cursor-not-allowed
-      "
-    >
-      
-    
-      {Icon && (
-        <Icon className="w-4 h-4 text-red-400 group-hover:scale-110 transition-transform duration-300" />
-      )}
-
-      <span>{link.name}</span>
-    </button>
+  key={link.href}
+  onClick={() => handleLinkClick(link.href)}
+  disabled={link.disabled}
+  className="group relative flex flex-row items-center justify-center gap-2 px-9.5 py-2.5 rounded-lg bg-white/5 backdrop-blur-xl
+    border
+    border-white/10
+    text-white/80
+    font-medium
+    tracking-wide
+    hover:bg-red-500/10
+    hover:border-red-500/40
+    hover:text-white
+    hover:-translate-y-1
+    hover:shadow-lg
+    hover:shadow-red-500/20
+    transition-all
+    duration-300
+    disabled:opacity-50
+    disabled:cursor-not-allowed
+  "
+>
+  {Icon && (
+    <Icon className="w-4 h-4 text-red-400 group-hover:scale-110 transition-transform duration-300 shrink-0" />
+  )}
+  <span>{link.name}</span>
+</button>
   );
 })}
     </div>
