c6ef15c465839748e445a0cf33c99ebe4e8e0998
Hamna-21 <hamnamushtaq97@gmail.com>
2026-08-07 13:49:16 +0500

    add prediction code

--- frontend/src/pages/Dashboard/Prediction/PredictionPage.tsx
+++ frontend/src/pages/Dashboard/Prediction/PredictionPage.tsx
@@ -1,6 +1,6 @@
 import { DashboardProps } from '../../../components/dashboard/types';
-import PredictionView from './components/PredictionView.tsx';
-import { usePredictionPageState } from './components/usePredictionPageState.tsx';
+import PredictionView from './components/PredictionView';
+import { usePredictionPageState } from './components/usePredictionPageState';
 
 export default function PredictionPage({ earthquakes, openPage }: DashboardProps) {
   return <PredictionView {...usePredictionPageState(earthquakes)} onClose={() => openPage('overview')} />;
